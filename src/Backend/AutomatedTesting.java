60a3ec4af8f0ce45c5d528f0a2258fd6d2ac220f
LizbtehTrujil786 <SpamEm674@gmail.com>
2023-04-26 00:15:02 -0500

    fixing one of the screens that was a bit crooked

--- src/Backend/AutomatedTesting.java
+++ src/Backend/AutomatedTesting.java
@@ -4,4 +4,9 @@ package Backend;
  * This class will run a number of automated scripts which test each of the programs classes
  * */
 public class AutomatedTesting {
+    public static void main(String[] args){
+
+
+
+    }
 }
